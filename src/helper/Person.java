229acfd06774906d9e9202602888906d8849a6f4
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-02-23 14:00:08 +0530

    getter and setter methods to access and initialize the values

--- src/helper/Person.java
+++ src/helper/Person.java
@@ -8,4 +8,36 @@ public class Person {
     private ArrayList<String> contactNumber;
     private String email;
 
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public ArrayList<String> getContactNumber(){
+        return contactNumber;
+    }
+
+    public void setContactNumber(ArrayList<String> contactNumber){
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
 }
