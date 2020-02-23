3b6a811d03f852606f9b2bca5d1f68a02e9d0b6f
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-02-23 14:37:08 +0530

    constructor to initialize the value of head as null

--- src/definition/Contact.java
+++ src/definition/Contact.java
@@ -10,6 +10,9 @@ public class Contact implements contactADT<Person> {
     Scanner sc = new Scanner( System.in );
     Node<Person> head;
 
+    public Contact(){
+        head = null;
+    }
 
     @Override
     public void add(Person data){
