1dbaad63bffe7af33f580e389888133e2929a296
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-02-23 13:57:44 +0530

    variables to add first name,lastName,multiple contact number and email

--- src/helper/Person.java
+++ src/helper/Person.java
@@ -1,5 +1,11 @@
 package helper;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> contactNumber;
+    private String email;
 
 }
