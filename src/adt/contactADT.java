01fd33e8fb0c5dbdd2352dca3f8fd18076f4bf77
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-02-23 14:22:15 +0530

    methods to add,remove,print and search the person from the list

--- src/adt/contactADT.java
+++ src/adt/contactADT.java
@@ -1,5 +1,30 @@
 package adt;
 
+import helper.Person;
+
 public interface contactADT {
+    /**
+     * this method is used to add a person details
+     *
+     * @param data information about a single person
+     */
+    public void add(Person data);
+
+    /**
+     * this method is used to remove the person details
+     */
+    public void remove();
+
+    /**
+     * this method is used to search a person from the list according to first name
+     *
+     * @param firstName first name of the person
+     */
+    public void search(String firstName);
+
+    /**
+     * this method is used to display all the elements in a list
+     */
+    public void viewAll();
 
 }
