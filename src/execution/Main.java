71fca99ca19fbc3e2b1beb495b6c24fb856d905e
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-02-23 16:26:57 +0530

    created an object of contact type

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,10 +1,13 @@
 package execution;
 
+import definition.Contact;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner( System.in );
+        Contact contact = new Contact();
 
     }
 }
